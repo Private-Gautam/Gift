562bcfdabad1a39150c56bd3c8eddd111b1e3d8a
Private-Gautam <132856797+Private-Gautam@users.noreply.github.com>
2023-05-08 01:55:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -289,7 +289,7 @@ const fx = new TextScramble(el);
 let counter = 0;
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 2000);
+    setTimeout(next, 2500);
   });
   counter = (counter + Math.floor(Math.random() * 20) + 1) % phrases.length;
 };
